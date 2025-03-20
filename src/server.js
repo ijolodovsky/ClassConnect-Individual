3c8a58094c5808f7cf853b55ed5aa1107d673976
ijolodovsky <ijolodovsky@fi.uba.ar>
2025-03-19 23:07:55 -0300

    Add Swagger documentation and CORS support; update dependencies

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger-output.json"); // Importa el archivo generado
+
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
+
 const courseController = require("./controllers/courseController");
 const { errorMiddleware } = require("./middleware/errorMiddleware");
 
@@ -10,6 +17,9 @@ app.use(express.json());
 app.use("/courses", courseController);
 
 app.use(errorMiddleware);
+
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.NODE_ENV === "test" ? process.env.TEST_PORT : process.env.PORT;
 
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require("swagger-autogen")();
+const dotenv = require("dotenv");
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./src/server.js", "./src/controllers/courseController.js"]; // Tus archivos de rutas
+
+const doc = {
+  info: {
+    title: "API de Cursos",
+    description: "Documentación de la API de cursos",
+  },
+  host: "localhost:3000",
+  schemes: ["http"],
+};
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
